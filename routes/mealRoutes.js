c5919891e97e8f196ed3c18299441972b6a671e1
kj8726 <kunal.jadhav8726@gmail.com>
2026-01-30 11:28:55 +0530

    working after atlas connected

--- routes/mealRoutes.js
+++ routes/mealRoutes.js
@@ -5,4 +5,12 @@ const mealController = require("../controllers/mealController");
 router.get("/", mealController.getHome);
 router.post("/add", mealController.addMeal);
 
+router.post("/delete/:id", async (req, res) => {
+  const Meal = require("../models/Meal");
+
+  await Meal.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
+
+
 module.exports = router;
